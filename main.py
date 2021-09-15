f87e607cb3b1831e76cd3fc2117424d0a4f6fe24
NolanThrondset2022 <816059@mystma.org>
2021-09-15 14:43:30 -0500

    added 4 new functions

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #define function
 def topFiveMovies():
-        #print first movie
+      #print first movie
     print("Blazing sadles")
 
       #print second movie
@@ -16,3 +16,74 @@ def topFiveMovies():
     print("Pacific rim")
 
 topFiveMovies()
+
+def topFivegames():
+    #print first game
+  print("1game")
+
+    #print second game
+  print("2game")
+
+    #print third game
+  print("3game")
+
+    #print fourth game
+  print("4game")
+
+    #print fifth game
+  print("5game")
+
+topFivegames()
+
+def topFiveBooks():
+  #print first book
+  print("1book")
+
+    #print second book
+  print("2book")
+
+    #print third book
+  print("3book")
+
+    #print fourth book
+  print("4book")
+
+    #print fifth book
+  print("5book")
+
+topFiveBooks()
+
+def topFiveShows():
+    #print first show
+  print("1show")
+
+    #print second show
+  print("2show")
+
+    #print third show
+  print("3show")
+    #print fourth show
+  print("4show")
+
+    #print fifth show
+  print("5show")
+
+topFiveShows()
+
+def topFiveTeachers():
+    #print first teacher
+  print("1teacher")
+
+    #print second teacher
+  print("2teacher")
+
+    #print third teacher
+  print("3teacher")
+
+    #print fourth teacher
+  print("4teacher")
+  
+    #print fifth teacher
+  print("5teacher")
+
+topFiveTeachers()
